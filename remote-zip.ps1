50427058e31115b34851e2e6b873a96dd1d9cc22
George Tong <george.tong@bkmg.com>
2023-05-02 11:55:25 -0400

    Update remote zip to include session invoke-command 3

--- remote-zip.ps1
+++ remote-zip.ps1
@@ -16,14 +16,14 @@ $targetzipfile = "$targetzip.$timestamp.zip"
 
 $display_action = "Compress $sourcefolder to server $server as $targetzipfile"
 $script_block = {param($a1, $a2) Compress-Archive -Path $a1 -DestinationPath $a2}
-$arguments = "$sourcefolder, $targetzipfile"
+
 Write-Output $display_action
 
 $credential = [PSCredential]::new($user_id, $password)
 $so = New-PSSessionOption -SkipCACheck -SkipCNCheck -SkipRevocationCheck
 
 $Session = New-PSSession -ComputerName $server -Credential $credential -SessionOption $so
-Invoke-Command -Session $Session -Scriptblock $script_block -ArgumentList $arguments
+Invoke-Command -Session $Session -Scriptblock $script_block -ArgumentList $sourcefolder,$targetzipfile
 Remove-PSSession $Session
 
 Write-Output "Zip file created."
