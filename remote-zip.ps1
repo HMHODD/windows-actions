43a36a30c139c18f5faf2ac7050a9b81db7161f5
George Tong <george.tong@bkmg.com>
2023-05-02 15:56:00 -0400

    Update remote zip to include session invoke-command 5

--- remote-zip.ps1
+++ remote-zip.ps1
@@ -30,4 +30,5 @@ Write-Output ""
 Write-Output "Deleting the source folder $sourcefolder from server $server"
 Invoke-Command -Session $Session -Scriptblock $rm_block -ArgumentList $sourcefolder
 Write-Output "Source folder $sourcefolder deleted."
+dir $targetzipfile*
 
